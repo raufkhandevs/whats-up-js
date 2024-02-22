1c94c003cfd46e90c07eace1c466966302cd43db
Rauf Khan <raufkhandevs@gmail.com>
2024-02-22 10:43:26 +0500

    release v1

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// Database connectivity
+mongoose.connect(
+  'mongodb+srv://rauf:mern@cluster0.flffz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+  }
+);
+var db = mongoose.connection;
+
+// Setting up error handler
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+  // we're connected!
+  console.log('MongoDB Connected...');
+});
+
+module.exports = db;
